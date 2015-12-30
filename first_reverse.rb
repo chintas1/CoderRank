170881d2b32ef87c5f0f84100d885e196c4499aa
chintas1 <chintas1@tcnj.edu>
2015-12-30 17:14:24 +0000

    Fixed syntax errors

--- first_reverse.rb
+++ first_reverse.rb
@@ -1,10 +1,9 @@
 def first_reverse(str)
-  rev_str == ""
+  rev_str = ""
   index = 0
-  while index<str.size
+  while index<str.size do
     rev_str << str[(-1)*(index + 1)]
+    index += 1
   end
   rev_str
 end
-
-first_reverse("Test case")
